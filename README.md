0f6aee300fc0acd097d7f30ce52957dab7f05219
biendo <aime@awelty.com>
2017-10-20 17:26:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# LIBORM
+> ORM that use the legacy libsqlite.
+
+Main classes: Orm, Command, Entity, Model etc...
+
+## Installation
+
+Node:
+
+```sh
+npm install liborm --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,44 @@
+/** @babel */
+import {
+    Command,
+    CommandReceiver,
+    SelectionCommand,
+    SQLiteCommandReceiver,
+    SQLiteCommand} from './lib/command';
+import {SQLiteResult} from './lib/cursor';
+import {Entity} from './lib/entity';
+import {Model,SQLiteModel} from './lib/model';
+import {Orm} from './lib/orm';
+import {
+    Selection,
+    SQLiteSelection,
+    Drop,
+    Insert,
+    Delete,
+    Update,
+    Select} from './lib/selection';
+
+export {
+    Command,
+    CommandReceiver,
+    SelectionCommand,
+    SQLiteCommandReceiver,
+    SQLiteCommand,
+
+    SQLiteResult,
+
+    Entity,
+
+    Model,
+    SQLiteModel,
+
+    Orm,
+
+    Selection,
+    SQLiteSelection,
+    Drop,
+    Insert,
+    Delete,
+    Update,
+    Select
+}
